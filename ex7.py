2e96301e03854ead91186311dfe343bc06750fd3
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-15 10:44:49 +0100

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,13 @@
+"""
+Hay cuatro errores en este código. Identifícalos.
+"""
+
+#Ejercicios del Capítulo 03
+#Bel Santandreu Nadal
+
+res = input("¿Cuál es el nombre del compañero de Sherlock Holmes?")
+if res == "Watson":
+    print("Correcto!")
+else:
+    print("Incorrecto! Es Watson.")
+
