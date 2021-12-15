09b5c103558a9d408341ea72d56040c7667f9511
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-15 10:40:18 +0100

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,10 @@
+"""
+¿Qué tres cosas están mal en este código?
+"""
+
+#Ejercicios del Capítulo 03
+#Bel Santandreu Nadal
+
+x = int(input("Introduce un número:"))
+if x == 3:
+    print("Has introducido el 3.") 
