eab62b99701a01491b45426c7278ba3b6acecf30
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-15 10:37:17 +0100

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,13 @@
+"""
+Este código que comprueba si x es un valor positivo. Hay dos cosas mal en él. Una impide que se ejecute, la otra es más sutil. Asegúrate de que la sentencia if funciona independientemente del valor que tome x. Identifícalas.
+"""
+
+#Ejercicios del Capítulo 03
+#Bel Santandreu Nadal
+
+x = float(input("Digues un nombre"))
+if x >= 0:
+    print("x es positivo.")
+else:
+    print("x no es positivo.")
+
