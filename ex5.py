7e6520d1b6a1e74ae91331bc22c4583adafd5d9a
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-16 11:39:40 +0100

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -2,7 +2,7 @@
 Este código que comprueba si x es un valor positivo. Hay dos cosas mal en él. Una impide que se ejecute, la otra es más sutil. Asegúrate de que la sentencia if funciona independientemente del valor que tome x. Identifícalas.
 """
 
-#Ejercicios del Capítulo 03
+#Ejercicio 5 del Capítulo 03
 #Bel Santandreu Nadal
 
 x = float(input("Digues un nombre"))
