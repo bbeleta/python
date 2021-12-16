ecaffd9ba3913b8617a22573940bd67fa376d51a
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-16 11:49:29 +0100

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,11 @@
+"""
+Este programa no funciona bien. ¿Por qué? 
+"""
+#Ejercicio 8 del capítulo 03
+#Bel Santandreu Nadal
+
+x = input("Com estàs avui?")
+if x == "Feliç" or x == "Be" or x == "Satisfet":
+    print ("M'alegra escoltar això!") 
+else:
+    print ("Milloret:)") 
