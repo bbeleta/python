67def1bb295c13654dc1fb8ac309b1e4d12c8596
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-17 09:16:25 +0100

    Add files via upload

---
+++ ex12.03.py
@@ -0,0 +1,22 @@
+"""
+¿Qué está mal en este código?
+"""
+#Ejercicio 10 del capítulo 03
+#Bel Santandreu Nadal
+
+print("Bienvenidos a la Ultra Trail!")
+ 
+print("A. Banquero")
+print("B. Carpintero")
+print("C. Granjero")
+ 
+entrada_usuario = input("¿Cuál es tu profesión?")
+ 
+if entrada_usuario == "A":
+    dinero = 100
+elif entrada_usuario == "B":
+    dinero = 70
+else:
+    dinero = 50
+     
+print("Genial! empezarás el juego con", dinero, "dólares.")
