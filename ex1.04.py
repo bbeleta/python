a9e96a129fe7632dc00d348691608331171d326f
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-17 09:33:29 +0100

    Add files via upload

---
+++ ex1.04.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa en Python que, usando un bucle for, imprima 10 veces tu nombre y la palabra ``Hecho'' al final.
+"""
+#Ejercicio 1 del capítulo 04
+#Bel Santandreu Nadal
+
+for i in range(10):
+    print ("Beleta")
+print ("Fet:)")
