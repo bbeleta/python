dc56300db7c54d9ac30c255e1d0cccf03216fe0f
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-20 10:23:48 +0100

    Add files via upload

---
+++ ex5.04.py
@@ -0,0 +1,17 @@
+"""
+Este programa contiene tres errores. Identifica cada uno de ellos. 
+"""
+#Ejercicio 5 del capítulo 04
+#Bel Santandreu Nadal
+
+print("Este programa recibe tres números y devuelve la suma.")
+total = 0
+ 
+for i in range(1):
+    x = int(input("Introduce un número: "))
+for i in range(1):
+    y = int(input("Introduce un número: "))
+for a in range(1):
+    z = int(input("Introduce un número: "))
+num = total + x + y + z
+print("El total es:", num)
---
+++ ex6.04.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa que imprima un número entero al azar entre 1 y 10
+"""
+#Ejercicio 6 del capítulo 04
+#Bel Santandreu Nadal
+
+x = input("Número aleatori entre 1 i 10")
+import random
+print(random.randrange(10))
