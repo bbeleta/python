a8a3ef3460c04e85b30d1aeb6a48b439fa034a6d
bbeleta <93370708+bbeleta@users.noreply.github.com>
2021-12-20 10:36:16 +0100

    Add files via upload

---
+++ ex7.04.py
@@ -0,0 +1,10 @@
+"""
+Escribe un programa que imprima un número real (float) al azar entre 1 y 10 (ambos incluidos). No vayas a cometer el error de generar un número aleatorio entre 0 y 10, en lugar de hacerlo entre 1 y 10.
+"""
+#Ejercicio 7 del capítulo 04
+#Bel Santandreu Nadal
+
+x = input("Número aleatori entre 1 i 10")
+import random
+print(float(random.uniform(1,10)))
+
