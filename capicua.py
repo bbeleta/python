25dee4d2963286aaf5d3266f8159a48f8666a0a4
bbeleta <93370708+bbeleta@users.noreply.github.com>
2022-01-26 10:19:30 +0100

    Create capicua.py

---
+++ capicua.py
@@ -0,0 +1,12 @@
+"""
+Nombres capicua
+"""
+
+#Bel Santandreu Nadal
+
+print("Introdueix una paraula qualsevol: ")
+paraula = input()
+if str(paraula) == str(paraula)[::-1] :
+  print("És capicua")
+else: 
+  print("No és capicua")
