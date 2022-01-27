2e95a3f7ce25c136488ad87d7c987f0106667bb8
bbeleta <93370708+bbeleta@users.noreply.github.com>
2022-01-27 11:33:49 +0100

    Add files via upload

---
+++ capicua.py
@@ -0,0 +1,25 @@
+"""
+Volem saber si una paraula és capicua:
+
+    entrada: “capicua”, sortida: no és capicua
+    entrada: “anna”, sortida: és capicua.
+"""
+#Ejercici capicua
+#Bel Santandreu Nadal
+
+print ("Digués una paraula:")
+paraula = input()
+
+
+x = len(paraula)-1
+capicua = 1 
+for i in range(len(paraula)):
+  if paraula[i] != paraula[x]:
+    capicua = 0
+  x = x-1
+if capicua == 1:
+  print("Es capicua")
+else:
+  print("No es capicua")
+  
+print(capicua)
